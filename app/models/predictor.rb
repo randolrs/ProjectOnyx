473c4a39a4c9fe01247ec27c2e24341685ff2f27
Randolrs <R.Shane.Randolph@gmail.com>
2015-07-01 18:27:30 -0500

    added default img

--- app/models/predictor.rb
+++ app/models/predictor.rb
@@ -7,7 +7,7 @@ class Predictor < ActiveRecord::Base
   has_many :prediction_games
   has_many :articles
 
-  has_attached_file :image, :styles => { :medium => "194x194>", :small => "70x70>", :thumb => "30x30>", :default_url => "images/blank_user_image.png" }
+  has_attached_file :image, :styles => { :medium => "194x194>", :small => "70x70>", :thumb => "30x30>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,12 +83,18 @@ Rails.application.configure do
 
 #set paperclip to upload images to amazon S3
   config.paperclip_defaults = {
-    :storage => :s3,
+    :storage => :s3, 
+    :default_url => '/images/missing_:style.png',
+    :path => "users/avatar/:style.:extension",
+    :bucket => "projectonyx",
+
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
+
+
 }
 
 end

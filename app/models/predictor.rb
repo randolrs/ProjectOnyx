eb13208879fc9f027415c4c496075e018e6df88e
Randolrs <R.Shane.Randolph@gmail.com>
2015-07-14 12:46:05 -0500

    more changes

--- app/models/predictor.rb
+++ app/models/predictor.rb
@@ -16,7 +16,7 @@ class Predictor < ActiveRecord::Base
 
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
-  validates :username, uniqueness: {message: "Username is taken"}
+  validates :username, uniqueness: {message: " is taken"}
 
 
 	def PredictionGame

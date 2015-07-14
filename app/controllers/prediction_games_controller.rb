47498dddec3eccf6abf133c6be23682e7a0644f6
Randolrs <R.Shane.Randolph@gmail.com>
2015-07-13 21:06:49 -0500

    outKast update

--- app/controllers/prediction_games_controller.rb
+++ app/controllers/prediction_games_controller.rb
@@ -225,7 +225,9 @@ class PredictionGamesController < ApplicationController
   # GET /prediction_games/new
   def new
 
-    @game = Game.find(params[:game])
+    @game = Game.find(params[:id])
+
+    @league = @game.league
 
     if @game.event_time > Time.now and @game.status == "o" and not current_predictor.prediction_games.where(:game_id => @game.id).present?
 
@@ -242,8 +244,6 @@ class PredictionGamesController < ApplicationController
       @prediction_game.event_time = @game.event_time
 
       @prediction_game.status = "o"
-
-      @prediction_game = @game.league
 
       @teama = Team.find_by_name(@game.teama)
       @teamh = Team.find_by_name(@game.teamh)

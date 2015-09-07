89edd24210e58f5c8840379807fdda05145c84a4
Randolrs <R.Shane.Randolph@gmail.com>
2015-09-07 15:36:44 -0500

    cave ethiopia

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -29,12 +29,14 @@ class AccountsController < ApplicationController
 
 		account_object = account.legal_entity
 
+		account_object.type = params[:payeeType]
+
 		unless params[:firstName] == ""
-		account_object.first_name = params[:firstName]
+			account_object.first_name = params[:firstName]
 		end
 
 		unless params[:lastName] == ""
-		account_object.last_name = params[:lastName]
+			account_object.last_name = params[:lastName]
 		end
 
 		@dob = params[:dobDate]
@@ -49,7 +51,32 @@ class AccountsController < ApplicationController
 
 		account.save
 
+		@accountType = account_object.type
+
 		@dob_object = account_object.dob
+
+		if @dob_object["month"].to_s.length == 1
+
+			@dob_month = "0" + @dob_object["month"].to_s
+
+		else
+
+			@dob_month = @dob_object["month"].to_s
+
+		end
+
+		if @dob_object["day"].to_s.length == 1 
+
+			@dob_day = "0" + @dob_object["day"].to_s
+
+		else
+
+			@dob_day = @dob_object["day"].to_s
+
+		end
+
+		@dob_year = @dob_object["year"].to_s
+
 
 		predictorpayeeedit_path(current_predictor.username)
 
@@ -67,9 +94,32 @@ class AccountsController < ApplicationController
 
 		@account_object = Stripe::Account.retrieve(@predictor.account_key_secret).legal_entity
 
+		@accountType = @account_object.type
 		@firstname = @account_object.first_name
 		@lastname = @account_object.last_name
 		@dob_object = @account_object.dob
+
+		if @dob_object["month"].to_s.length == 1
+
+			@dob_month = "0" + @dob_object["month"].to_s
+
+		else
+
+			@dob_month = @dob_object["month"].to_s
+
+		end
+
+		if @dob_object["day"].to_s.length == 1 
+
+			@dob_day = "0" + @dob_object["day"].to_s
+
+		else
+
+			@dob_day = @dob_object["day"].to_s
+
+		end
+
+		@dob_year = @dob_object["year"].to_s
 
 	end
 

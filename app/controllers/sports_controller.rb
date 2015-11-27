a613f0c1c9eb3c55ce44f077922974fb594f4985
randolrs <r.shane.randolph@gmail.com>
2015-11-27 17:58:15 -0600

    update sport model

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -69,6 +69,8 @@ class SportsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def sport_params
-      params.require(:sport).permit(:subcat, :image, :banner_image)
+      params.require(:sport).permit(:subcat, :image, :banner_image, :image_file_name, :image_content_type, :image_file_size, :image_updated_at)
+
+      
     end
 end
--- app/models/sport.rb
+++ app/models/sport.rb
@@ -5,10 +5,12 @@ class Sport < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
 						:s3_protocol => :https
-	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
-	has_attached_file :banner_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+	
+    has_attached_file :banner_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
 						:s3_protocol => :https
+
 	validates_attachment_content_type :banner_image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 	def recent_prediction_games

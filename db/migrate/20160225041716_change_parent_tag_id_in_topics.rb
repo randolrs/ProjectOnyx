4f03c77cca423a08e18dfc98733389686e80d2e7
randolrs <r.shane.randolph@gmail.com>
2016-02-24 22:41:26 -0600

    another change to migration

--- db/migrate/20160225041716_change_parent_tag_id_in_topics.rb
+++ db/migrate/20160225041716_change_parent_tag_id_in_topics.rb
@@ -1,5 +1,9 @@
 class ChangeParentTagIdInTopics < ActiveRecord::Migration
 	def change
-    	change_column :topics, :parent_tag_id, 'integer USING CAST("parent_tag_id" AS integer)'
+    	connection.execute(%q{
+    		alter table topics
+    		alter column parent_tag_id
+    		type integer using cast(number as integer)
+  			})
   	end
 end

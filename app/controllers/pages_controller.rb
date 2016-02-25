3ec956546fbaade6dcbc7dc2ecb1c8197c84925e
randolrs <r.shane.randolph@gmail.com>
2016-02-24 22:53:21 -0600

    whoa

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
 
     @page = "dashboard"
 
-    @parent_tag_id = Topic.find_by_name("All").id
+    @parent_tag_id = Topic.find_by_name("All").id.to_s
     
     @featured_topics = Topic.all.where("parent_tag_id = :all_id", {:all_id => @parent_tag_id})
 
--- db/migrate/20160225041716_change_parent_tag_id_in_topics.rb
+++ db/migrate/20160225041716_change_parent_tag_id_in_topics.rb
@@ -1,9 +1,9 @@
 class ChangeParentTagIdInTopics < ActiveRecord::Migration
 	def change
-    	connection.execute(%q{
-    		alter table topics
-    		alter column parent_tag_id
-    		type integer using cast(parent_tag_id as integer)
-  			})
+    	# connection.execute(%q{
+    	# 	alter table topics
+    	# 	alter column parent_tag_id
+    	# 	type integer using cast(parent_tag_id as integer)
+  			# })
   	end
 end
